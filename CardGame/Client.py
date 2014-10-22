b7871ab9a68a73304fbcda5b24e2d0550d9e3f44
Christopher Kawasaki <cmkawasaki@yahoo.com>
2014-10-21 17:18:09 -0700

    Commiting Initial Client Script, containing first thoughts of Game.

---
+++ CardGame/Client.py
@@ -0,0 +1,5 @@
+#Begin Game with Title Announcement.
+#Request Number of Players (2-4 are supported)
+#Set up Central Purchasing area - Love Cards, Chambermaids, Maids, and so on.
+#Set up Players' Decks, which include 7 '1 Love' cards and 3 'Chambermaid' cards
+#Each Player's data consists of Deck, Discard Pile, and Private Chambers
